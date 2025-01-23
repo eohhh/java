8d1b52e045f7481f224f853706e499b9025a4d5e
김어진 <asas0066@naver.com>
2025-01-23 21:23:59 +0900

    ㅋㅋㅋ

--- HelloWorld/src/com/yedam/reference/Book.java
+++ HelloWorld/src/com/yedam/reference/Book.java
@@ -5,13 +5,22 @@ public class Book {
 	String name;
 	String media;
 	int won;
-	int bookRE;
-	
+	String bookRE[];
+	String search;
+	String search1;
+	String split;
 
+	Book() {
+		
+	}
 	Book(String bookName, String name, String media, int won) {
 		this.bookName = bookName;
 		this.name = name;
 		this.media = media;
 		this.won = won;
+	}
+		void printInfo() {
+			System.out.printf("이름은 %s, 영어점수 %d, 수학점수 %d\n", bookName, name, media);
+		
 	}
 }
--- HelloWorld/src/com/yedam/reference/BookExe.java
+++ HelloWorld/src/com/yedam/reference/BookExe.java
@@ -11,9 +11,33 @@ public class BookExe {
 		// 1.전체목록 2.도서등록 3.조회(출판사명을 넣으면 조회) 9.종료
 		Scanner scn = new Scanner(System.in);
 		Book[] bookRe = new Book[100];
-		Book bookRe[0] = new Book("혼공자", "신용권", "한빛미디어", 28000);
-		Book bookRe2 = new Book("혼공", "신용", "한빛미디", 2800);
-		Book bookRe3 = new Book("혼", "신", "한빛미", 280);
+//		Book bookRe1 = new Book("혼공자", "신용권", "한빛미디어", 28000);
+//		Book bookRe2 = new Book("혼공", "신용", "한빛미디", 2800);
+//		Book bookRe3 = new Book("혼", "신", "한빛미", 280);
+		
+		Book bk1 = new Book();
+		bk1.bookName = "혼공자";
+		bk1.name = "신용권";
+		bk1.media = "한빛미디어";
+		bk1.won = 28000;
+		
+		Book bk2 = new Book();
+		bk2.bookName = "혼공";
+		bk2.name = "신용";
+		bk2.media ="한빛미디";
+		bk2.won = 2800;
+		
+		Book bk3 = new Book();
+		bk3.bookName = "혼";
+		bk3.name = "신";
+		bk3.media = "한빛미";
+		bk3.won = 280;
+		
+		bookRe[0] = bk1;
+		bookRe[1] = bk2;
+		bookRe[2] = bk3;
+		
+		
 		boolean run = true;
 		
 		while(run) {
@@ -30,24 +54,36 @@ public class BookExe {
 				 }
 				 break;
 			 case 2:
-//				 int search = Integer.parseInt(scn.nextLine());
+				 int search = Integer.parseInt(scn.nextLine());
 				 for (int i = 0; i < bookRe.length; i++) {
 					 if (bookRe[i] == null) {
+//						 if (search.equals(bookRe[i]).split(",")[])
 //						 bookRe[i] = scn.nextLine();
+//						 search.equals(bookRe[i]).split(",")[]
+						 
 						 break;
 					 }
 				 }
 				 break;
 			 case 3:
+				 String search1 = scn.nextLine();
+				 for (int i = 0; i < bookRe.length; i++) {
+					 if (bookRe[i] != null) {
+						 if (search1.equals(bookRe[i].split(",")[0])) {
+						 
+					 }	 bookRe[i] = null;
+						 }
+					 }
 				 break;
 			 case 9:
 				 System.out.println("시스템을 종료합니다.");
 				 run = false;
+				 break;
 				 default:
 					 System.out.println("메뉴를 다시 선택하세요.");
-			
-			 } // end of switch.
-
-		} // end of while.
-	} // end of main.
-} // end of class.
+		
+			 }
+			 } 
+		} 
+	} 
+ 
